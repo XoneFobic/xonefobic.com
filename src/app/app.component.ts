e260bdc9055d60ef3c96d311d01639320dd69d93
XoneFobic <xonefobic@gmail.com>
2021-02-03 12:54:42 +0100

    feat(analytics): add google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component }             from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+
+// tslint:disable-next-line:ban-types
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.scss' ]
 })
-export class AppComponent {}
+export class AppComponent {
+  public constructor (public router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        gtag('config', 'G-3XJRN1K6BS', {
+          page_path: event.urlAfterRedirects
+        });
+      }
+    });
+  }
+}
